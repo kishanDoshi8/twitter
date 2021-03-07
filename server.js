aa8ce4ddc97ef86a6b94ea0e7417f9c182a4081e
Kishan Doshi <kishan.doshi88@gmail.com>
2021-03-07 03:06:10 -0500

    server and db initial setup

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+require("dotenv").config();
+
+const app = express();
+
+app.use(express.json());
+
+// DB Configuratoin
+const mongoURI = process.env.mongoURI;
+mongoose.connect(mongoURI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false
+    })
+    .then(() => console.log('MongoDB Connected...'))
+    .catch(err => console.log(err));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on post: ${port}`));
